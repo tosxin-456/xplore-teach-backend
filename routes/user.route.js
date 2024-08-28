e94e38e97b3402fb3312fe564cb489f576a7a8d6
tosxin-456 <tosinekshally@gmail.com>
2024-08-28 16:35:26 +0100

    test

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,7 @@ router.get("/",  async(req, res) => {
         message:'we are using the user route. '
     })
 });
-
+ // const salt = await bcrypt.genSalt(10);
+      // const hashedPassword = await bcrypt.hash(password, salt);
 
 module.exports = router
